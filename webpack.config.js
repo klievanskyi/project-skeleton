5dda1fbb7eb2012079ac24025a0c21ecdb4c7861
NickoKlevanski <nikolai.klevanski@gmail.com>
2019-11-22 18:29:58 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -16,9 +17,18 @@ module.exports = {
         loader: 'babel-loader',
         exclude: '/node_modules/',
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   devServer: {
     overlay: true,
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 };
